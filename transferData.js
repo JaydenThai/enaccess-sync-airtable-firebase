6b150ca6cd222ab848973b3cc5b4ca249432a1cf
YiJieChuah <s3847905@student.rmit.edu.au>
2024-05-21 11:18:45 +1000

    refined script for better accuracy

--- transferData.js
+++ transferData.js
@@ -1,12 +1,12 @@
 // Import required modules
-require('dotenv').config();
-const Airtable = require('airtable');
-const axios = require('axios');
-const admin = require('firebase-admin');
-const { v4: uuidv4 } = require('uuid');
-const sharp = require('sharp');
-const { Readable } = require('stream');
-const readline = require('readline');
+require("dotenv").config();
+const Airtable = require("airtable");
+const axios = require("axios");
+const admin = require("firebase-admin");
+const { v4: uuidv4 } = require("uuid");
+const sharp = require("sharp");
+const { Readable } = require("stream");
+const readline = require("readline");
 const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_PATH);
 
 // Initialize Airtable
@@ -26,31 +26,49 @@ const collectionName = process.env.FIREBASE_COLLECTION_NAME;
 
 // Initialize Google Maps API
 const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
-const GOOGLE_MAPS_PLACES_API_BASE_URL = 'https://maps.googleapis.com/maps/api/place';
+const GOOGLE_MAPS_PLACES_API_BASE_URL =
+  "https://maps.googleapis.com/maps/api/place";
 
 // Function to fetch place ID from Google Maps using text search API
-const fetchPlaceID = async (location, query) => {
-    const apiUrl = `${GOOGLE_MAPS_PLACES_API_BASE_URL}/textsearch/json?key=${GOOGLE_MAPS_API_KEY}&input=${query}`
-      + `&location=${location.latitude},${location.longitude}`
-      + `&types=restaurant|cafe|food`
-      + `&radius=5000`;
+const fetchPlaceID = async (name, address) => {
+  /***
+   *    res = {
+        id: place.place_id,
+        lat: place.geometry.location.lat,
+        lng: place.geometry.location.lng,
+        types: place.types // array
+      };
+   */
+  const apiUrl =
+    `${GOOGLE_MAPS_PLACES_API_BASE_URL}/textsearch/json?key=${GOOGLE_MAPS_API_KEY}&input=${name} ${address}`;
+
+  const res = await axios.get(apiUrl);
+
+  if (res) {
+    const {
+      data: { results },
+    } = res;
+
+    if (results[0]) {
+      place = results[0];
+      console.log(`\nfor QUERY: ${name} ${address} -> found \n\tname : ${place.name} \n\taddress : ${place.formatted_address} \n\tid: ${place.place_id}`)
   
-    const res = await axios.get(apiUrl);
+      return {
+        id: place.place_id,
+        lat: place.geometry.location.lat,
+        lng: place.geometry.location.lng,
+        types: place.types, // array
+      };
+    } else {
 
-    if (res) {
-      const {
-        data: { results },
-      } = res;
-      console.log("place id is:", results[0]?.place_id);
-      console.log("for place", query);
-
-      return results.length > 0 ? results[0].place_id : null;
+      console.log(`for QUERY: ${name} ${address} -> found nothing :c`)
+      return null;
     }
-  };
+  }
+};
 
 // functions to write photos to storage
 const storage = admin.storage().bucket();
-
 
 async function checkIfPhotoExists(filename) {
   try {
@@ -69,16 +87,16 @@ async function getExistingPhotoObject(filename) {
 
     if (file) {
       const [signedUrl] = await file.getSignedUrl({
-        action: 'read',
-        expires: '03-17-2025',
+        action: "read",
+        expires: "03-17-2025",
       });
 
       return {
         url: signedUrl,
-        uuid: file.name.split('/')[1],
+        uuid: file.name.split("/")[1],
       };
     } else {
-      console.error('File not found.');
+      console.error("File not found.");
       return null;
     }
   } catch (error) {
@@ -91,16 +109,16 @@ async function uploadPhotoAsync(url) {
   try {
     // Download the photo from the given URL
     const response = await axios({
-      method: 'GET',
+      method: "GET",
       url,
-      responseType: 'arraybuffer', // Change responseType to arraybuffer
+      responseType: "arraybuffer", // Change responseType to arraybuffer
     });
 
     // Check if the content type is image/jpeg
-    const contentType = response.headers['content-type'];
+    const contentType = response.headers["content-type"];
     let imageData = response.data;
 
-    if (contentType !== 'image/jpeg') {
+    if (contentType !== "image/jpeg") {
       // Convert non-JPEG images to JPEG
       imageData = await sharp(imageData).jpeg().toBuffer();
     }
@@ -113,22 +131,19 @@ async function uploadPhotoAsync(url) {
     const file = storage.file(filename);
     const writeStreamOptions = {
       metadata: {
-        contentType: 'image/jpeg', // Set content type to image/jpeg
+        contentType: "image/jpeg", // Set content type to image/jpeg
       },
     };
     await new Promise((resolve, reject) => {
       const writeStream = file.createWriteStream(writeStreamOptions);
       const readStream = Readable.from(imageData); // Use Readable.from() method
-      readStream
-        .pipe(writeStream)
-        .on('error', reject)
-        .on('finish', resolve);
+      readStream.pipe(writeStream).on("error", reject).on("finish", resolve);
     });
 
     // Generate a signed URL for the uploaded photo
     const [signedUrl] = await file.getSignedUrl({
-      action: 'read',
-      expires: '03-17-2025',
+      action: "read",
+      expires: "03-17-2025",
     });
 
     return {
@@ -144,15 +159,15 @@ async function uploadPhotoAsync(url) {
 function transformResponse(response) {
   switch (response) {
     case "Yes":
-      return 'yes';
+      return "yes";
     case "No":
-      return 'no';
+      return "no";
     case "Unsure":
-      return 'unsure';
+      return "unsure";
     case "N/A no steps on entry":
-      return 'na';
+      return "na";
     default:
-      return 'unsure';
+      return "unsure";
   }
 }
 
@@ -190,146 +205,172 @@ const defaultTally = {
   na: 0,
   no: 0,
   yes: 0,
-  unsure: 0
-}
+  unsure: 0,
+};
 
-async function addPlace(placeId, placeName) {
-  firestore.collection('places').doc(placeId).set({
-    name: placeName,
-    address: "",
-    hash: "",
-    lat: 0,
-    lng: 0,
-    recentReviews: [],
-    accessibleParkingTally: defaultTally,
-    accessibleToiletTally: defaultTally,
-    movementSpaceTally: defaultTally,
-    outdoorEatingTally: defaultTally,
-    stableRampTally: defaultTally,
-    wcFitHeight: 0,
-    restaurantFeatures: {
-      accessibleParking: false,
-      accessibleToilet: false,
-      movementSpace: false,
-      outdoorEating: false,
-      seatingOptions: false,
-      stepFree: false
-    },
-    stepsPoll: defaultTally,
-    types: []
-  }, {merge: true})
+async function addPlace(placeId, placeName, lat, lng, types, address) {
+  firestore
+    .collection("places")
+    .doc(placeId)
+    .set(
+      {
+        name: placeName,
+        address: address,
+        hash: "",
+        lat: lat,
+        lng: lng,
+        recentReviews: [],
+        accessibleParkingTally: defaultTally,
+        accessibleToiletTally: defaultTally,
+        movementSpaceTally: defaultTally,
+        outdoorEatingTally: defaultTally,
+        stableRampTally: defaultTally,
+        restaurantFeatures: {
+          accessibleParking: false,
+          accessibleToilet: false,
+          movementSpace: false,
+          outdoorEating: false,
+          seatingOptions: false,
+          stepFree: false,
+        },
+        stepsPoll: defaultTally,
+        types: types,
+      },
+      { merge: true }
+    );
 }
 
 // Function to transfer data from Airtable to Firebase
 const transferData = async () => {
-    try {
-      let iteratereview = 0
-      const records = await base(tableName)
-        .select()
-        .all();
-  
-      const batch = firestore.batch();
-  
-      for (const record of records) {
-        const address = record.get('Address') + " " + record.get("Suburb")
-        const location = { // Replace with actual latitude and longitude values
-          latitude: 0,
-          longitude: 0,
-        };
-  
-        let placeID = await fetchPlaceID(location, address);
-        let placeRating = record.get('Overall rating (out of 5)')
-        let placeName = record.get("Restaurant Name")
+  try {
+    let iteratereview = 0;
+    const records = await base(tableName).select().all();
 
-        // If placeID is not found, ask the user to enter it manually
-        console.log("for double checking name is:", placeName);
+    const batch = firestore.batch();
 
-        /*if (!placeID) {
+    // test to make sure we dont spend 1 million of sabrinas dollars
+    const n = 3;
+    const shortRecords = records.slice(0, n);
+
+    for (const record of records) {
+      const address = record.get("Restaurant Address");
+      const name = record.get("Restaurant Name");
+
+      // res = {
+      //   id: place.place_id,
+      //   lat: place.geometry.location.lat,
+      //   lng: place.geometry.location.lng,
+      //   types: place.types // array
+      // };
+
+      let res = await fetchPlaceID(name, address);
+  
+
+      let placeRating = record.get("Overall rating (out of 5)");
+      // let placeName = record.get("Restaurant Name")
+
+      // If placeID is not found, ask the user to enter it manually
+      // console.log("for double checking name is:", placeName);
+
+      /*if (!placeID) {
           placeID = await getManualInput(
             'Place ID not found. Please enter it manually: '
           );
         }*/
-        //skip record completely if Place ID not found
-        if (!placeID) {
-          console.log('Place ID not found. Skipping this place...');
-          continue;
-        }
-        
-
-
-        // If placeRating is not found, ask the user to enter it manually
-        var newplaceRating = 5;
-        console.log("for reference, comment is:", record.get("Other comment"))
-        // If placeRating is not found, set it to 5 automatically
-        // modified to automatically give 5 instead of getting input to check
-        if (placeRating === undefined) {
-          newplaceRating = 5;
-        }
-  
-        if (placeID) {
-          addPlace(placeID, placeName)
-
-          //Allocate 
-
-          const photoAttachments = record.get("Any photos") || [];
-          console.log("ATTACHMENTS", photoAttachments)
-
-          const photoObjects = await Promise.all(
-            photoAttachments.map(async (attachment) => {
-              const { url, filename } = attachment;
-  
-              // Check if the photo is already in Firebase Storage using its filename or UUID
-              const photoExists = await checkIfPhotoExists(filename); // Implement this function
-  
-              if (!photoExists) {
-                // Upload the photo if it's not in Firebase Storage
-                const photoObject = await uploadPhotoAsync(url);
-                return photoObject;
-              } else {
-                // Use the existing photo in Firebase Storage
-                const existingPhotoObject = await getExistingPhotoObject(filename); // Implement this function
-                return existingPhotoObject;
-              }
-            })
-          );
-
-          const review = {
-            userUid: "6BGlA6AO35gWLwZ6TuAxiyzGQDp1",
-            userName: record.get('Mailing List - First Name') ?? "Legacy Review",
-            rating: record.get('Overall rating (out of 5)') ?? newplaceRating,
-            stepsOnEntry: parseInt(record.get('Steps on Entry')) ?? 0,
-            hasStableRamp: transformResponse(record.get('Stable ramp')),
-            wcFitsTable: transformResponse(record.get('Table Access')),
-            hasMovementSpace: transformResponse(record.get('Sufficient Movement Space')),
-            hasAccessibleToilet: transformResponse(record.get('Wheelchair Accessible Toilet')),
-            hasAccessibleParking: transformResponse(record.get('Accessible Parking')),
-            hasOutdoorEating: transformResponse(record.get('Outdoor Eating')),
-            comment: record.get('Other comment') ?? "",
-            createdAt: new Date(2021, 1),
-            photos: photoObjects // Add photos from the Airtable record if needed
-          };
-          
-          console.log("review set: ", iteratereview)
-          console.log(review)
-          /*await waitForUserInput(
-            'Press ENTER to submit the review to Firebase, or CTRL+C to exit...'
-          );*/
-  
-          const reviewsCollection = firestore.collection(collectionName).doc(placeID).collection("reviews");
-          const newReviewDoc = reviewsCollection.doc();
-          //batch.set(newReviewDoc, review);
-          await newReviewDoc.set(review);
-          
-          iteratereview++
-        }
+      //skip record completely if Place ID not found
+      if (!res) {
+        //console.log("Place ID not found. Skipping this place...");
+        continue;
       }
+
+      placeID = res.id;
+      lat = res.lat;
+      lng = res.lng;
+      types = res.types;
+
+      // If placeRating is not found, ask the user to enter it manually
+      var newplaceRating = 5;
+      //console.log("for reference, comment is:", record.get("Other comment"))
+      // If placeRating is not found, set it to 5 automatically
+      // modified to automatically give 5 instead of getting input to check
+      if (placeRating === undefined) {
+        newplaceRating = 5;
+      }
+
+      if (placeID) {
+        addPlace(placeID, name, lat, lng, types, address);
+
+        //Allocate
+
+        const photoAttachments = record.get("Any photos") || [];
+        //console.log("ATTACHMENTS", photoAttachments)
+
+        const photoObjects = await Promise.all(
+          photoAttachments.map(async (attachment) => {
+            const { url, filename } = attachment;
+
+            // Check if the photo is already in Firebase Storage using its filename or UUID
+            const photoExists = await checkIfPhotoExists(filename); // Implement this function
+
+            if (!photoExists) {
+              // Upload the photo if it's not in Firebase Storage
+              const photoObject = await uploadPhotoAsync(url);
+              return photoObject;
+            } else {
+              // Use the existing photo in Firebase Storage
+              const existingPhotoObject = await getExistingPhotoObject(
+                filename
+              ); // Implement this function
+              return existingPhotoObject;
+            }
+          })
+        );
+
+        const review = {
+          userUid: "6BGlA6AO35gWLwZ6TuAxiyzGQDp1",
+          userName: record.get("Mailing List - First Name") ?? "Legacy Review",
+          rating: record.get("Overall rating (out of 5)") ?? newplaceRating,
+          stepsOnEntry: parseInt(record.get("Steps on Entry")) ?? 0,
+          hasStableRamp: transformResponse(record.get("Stable ramp")),
+          wcFitsTable: transformResponse(record.get("Table Access")),
+          hasMovementSpace: transformResponse(
+            record.get("Sufficient Movement Space")
+          ),
+          hasAccessibleToilet: transformResponse(
+            record.get("Wheelchair Accessible Toilet")
+          ),
+          hasAccessibleParking: transformResponse(
+            record.get("Accessible Parking")
+          ),
+          hasOutdoorEating: transformResponse(record.get("Outdoor Eating")),
+          comment: record.get("Other comment") ?? "",
+          createdAt: new Date(2021, 1),
+          photos: photoObjects, // Add photos from the Airtable record if needed
+        };
+
+           /*await waitForUserInput(
+            'Press ENTER to submit the review to Firebase, or CTRL+C to exit...'
+          );*/   //console.log(review)
   
-      //await batch.commit();
-      console.log('Data transferred successfully from Airtable to Firebase.');
-    } catch (error) {
-      console.error('Error transferring data:', error);
+
+        const reviewsCollection = firestore
+          .collection(collectionName)
+          .doc(placeID)
+          .collection("reviews");
+        const newReviewDoc = reviewsCollection.doc();
+        //batch.set(newReviewDoc, review);
+        await newReviewDoc.set(review);
+
+        iteratereview++;
+      }
     }
-  };
+
+    //await batch.commit();
+    console.log("\nData transferred successfully from Airtable to Firebase.");
+  } catch (error) {
+    console.error("\nError transferring data:", error);
+  }
+};
 
 // Execute the transfer
 transferData();
